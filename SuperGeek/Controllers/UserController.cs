859088132e98e6ada0510e960d3a137cc7a52758
b4shtirk1n <wowan4512@gmail.com>
2023-11-13 11:00:20 +0500

    first commit

---
+++ SuperGeek/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperGeek.Models;
+
+namespace SuperGeek.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public async Task<IActionResult> SingUp(UserDTO user)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SuperGeek/Models/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperGeek.Models
+{
+    public class UserDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Phone { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        public string Patronymic { get; set; } = null!;
+    }
+}
